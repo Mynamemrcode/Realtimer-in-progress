0f95c6651521744fc63496e6711cc1e559bdc5b3
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-08-03 18:39:58 -0700

    Add files via upload

--- Thebrain.js
+++ Thebrain.js
@@ -6,7 +6,23 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    classifier = ml5.imageClassify('https://teachablemachine.withgoogle.com/models/iNZXwoBiw/model.json', Loader);
 }
 function draw() {
-image(video,0,0,800,500)
+image(video,0,0,800,500);
+classifier.classify(video, Result);
+}
+function Loader() {
+    console.log("Loaded");
+}
+
+function Result(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("obj_268").innerHTML = results[0].label;
+        document.getElementById("aceuracy").innerHTML = results[0].confidence.toFixed(1);
+    }
 }
